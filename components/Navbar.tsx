44aa60fb49a86fee9c948f6cdf88b2ce79b1b008
Yash Bhalgat <yashbhalgat@Yashs-MacBook-Air.local>
2024-01-22 15:20:46 -0800

    Implemented Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,30 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
+
+const navIcons = [
+  {src: '/assets/icons/search.svg', alt: 'Search'},
+  {src: '/assets/icons/black-heart.svg', alt: 'Favourite'},
+  {src: '/assets/icons/user.svg', alt: 'Profile'},
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='w-full'>
+      <nav className='nav'>
+        <Link href="/" className='flex items-center gap-1'>
+          <Image src="/assets/icons/logo.svg" width={27} height={27} alt='Logo'/>
+          <p className='nav-logo'>
+            Price<span className='text-primary'>Wise</span>
+          </p>
+        </Link>
+        <div className='flex items-center gap-5'>
+          {navIcons.map((icon) => (
+            <Image src={icon.src} key={icon.alt} alt={icon.alt} width={28} height={28} className='object-contain'/>
+          ))}
+        </div>
+      </nav>
+    </header>
   )
 }
 
